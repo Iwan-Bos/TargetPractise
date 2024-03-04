aa9a3b11eff6c7fe94f15a4e0c134b4d073c3d0b
Iwan-Bos <iwanbos05alt@gmail.com>
2024-03-04 10:21:16 +0100

    add LevelManager

---
+++ src/levelManager.cpp
@@ -0,0 +1,17 @@
+#include <raylib-cpp.hpp>
+#include <iostream>
+#include "targetPractise.hpp"
+#include "levelManager.hpp"
+
+class LevelManager
+{
+
+};
+
+LevelManager::LevelManager(/* args */)
+{
+}
+
+LevelManager::~LevelManager()
+{
+}
